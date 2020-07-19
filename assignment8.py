27a74498c910d0961c4adb96b337b33f91ab9fe1
ally <ally@ally-iMac.local>
2020-07-19 21:59:43 +0300

    leap year fonksiyonsuz

---
+++ assignment8.py
@@ -0,0 +1,12 @@
+while True:
+    leap = int(input("Enter a year(yyyy): "))
+    if (leap % 4):
+        print(f"{leap} is not a leap year") 
+    elif not (leap % 100) and (leap % 400):
+        print(f"{leap} is not a leap year") 
+    elif not (leap % 400):
+        print(f"{leap} is a leap year") 
+        break
+    elif not (leap % 4) and (leap % 100):
+        print(f"{leap} is a leap year")
+        break 
